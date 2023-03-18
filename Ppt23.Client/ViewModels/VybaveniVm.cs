9270b1057a80106376b43776b37aab6eab9d909f
Jan Pecha <pechaj10@cvut.cz>
2023-03-18 19:12:59 +0100

    cv04 done

--- Ppt23.Client/ViewModels/VybaveniVm.cs
+++ Ppt23.Client/ViewModels/VybaveniVm.cs
@@ -1,35 +1,54 @@
-﻿namespace Ppt23.Client.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Ppt23.Client.ViewModels;
 
 public class VybaveniVm
 {
+    [Required(ErrorMessage = "Pole nesmí být prázdné")]
+    [MinLength(5, ErrorMessage = "Délka u pole \"{0}\" musí být alespoň {1} znaků")]
+    [Display(Name = "Název")]
     public string NAME { get; set; }
     public DateTime DATEBUY { get; set; }
-    public DateTime LASTREV { get; set; }
-    public bool IsRevNeeded { get => LASTREV + new TimeSpan(1095, 0, 0, 0) < DateTime.Now; }
 
+    [CustomValidation(typeof(VybaveniVm),nameof(validation))]
+    public DateTime LASTREV { get; set; }
+    public bool IsRevNeeded { get => DateTime.Now.AddYears(-2) > LASTREV; }
+    public bool IsInEditMode { get; set; }
+
+    [Required(ErrorMessage = "Pole Cena nesmí být prázdné")]
+    [Range(0, 10000000, ErrorMessage = "Cena musí být mezi 0 až 10,000,000")]
+    public int CENA { get; set; }
+
+    
     public VybaveniVm()
     {
         DateTime od = new DateTime(2010, 01, 01);
-        this.NAME = randName();
+        this.NAME = randName(10);
         this.DATEBUY = GetRandomDate(od, DateTime.Now);
         this.LASTREV = GetRandomDate(DATEBUY, DateTime.Now);
+        this.IsInEditMode = false;
+        this.CENA = Random.Shared.Next(10000000);
     }
-    public string randName()
+    public VybaveniVm Copy()
     {
-        Random rand = new Random();
-
-        String str = "abcdefghijklmnopqrstuvwxyz";
-        int size = 10;
-
-        String ran = "";
-
-        for (int i = 0; i < size; i++)
-        {
-            int x = rand.Next(26);
-            ran = ran + str[x];
-        }
-        return ran;
+        VybaveniVm to = new();
+        to.DATEBUY = DATEBUY;
+        to.LASTREV = LASTREV;
+        to.IsInEditMode = IsInEditMode;
+        to.NAME = NAME;
+        to.CENA = CENA;
+        return to;
     }
+    public void MapTo(VybaveniVm? to)
+    {
+        if (to == null) return;
+        to.DATEBUY = DATEBUY;
+        to.LASTREV = LASTREV;
+        to.NAME = NAME;
+        to.CENA = CENA;
+    }
+    public string randName(int length) => new(Enumerable.Range(0, length).Select(_ => (char)Random.Shared.Next('a', 'z')).ToArray());
 
     public static DateTime GetRandomDate(DateTime from, DateTime to)
     {
@@ -40,5 +59,16 @@ public class VybaveniVm
 
         return from + randTimeSpan;
     }
-    
+
+    public static ValidationResult? validation(DateTime lastRev, ValidationContext validationContext)
+    {
+        var vm = (VybaveniVm)validationContext.ObjectInstance;
+
+        if (lastRev < vm.DATEBUY)
+        {
+            return new ValidationResult("Poslední revize nesmí být dříve než koupě");
+        }
+
+        return ValidationResult.Success;
+    }
 }
