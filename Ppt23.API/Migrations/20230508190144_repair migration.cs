e1cc92461238774d93ec7d2c83c2cf28b57f0f25
Jan Pecha <honzik947@seznam.cz>
2023-05-08 21:02:42 +0200

    db fix?

---
+++ Ppt23.API/Migrations/20230508190144_repair migration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ppt23.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class repairmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
